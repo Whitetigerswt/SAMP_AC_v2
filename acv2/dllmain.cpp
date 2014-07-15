2bf0802d6adcfbd3eead630fad21ddaba970a84d
Whitetigerswt <spamsender123@yahoo.com>
2014-07-15 03:58:57 -0400

    Statically link releases

--- acv2/dllmain.cpp
+++ acv2/dllmain.cpp
@@ -3,8 +3,6 @@
 #include "CLoader.h"
 #include "../Shared/Boost/thread.hpp"
 
-void WINAPI Load();
-
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -13,17 +11,8 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:		
-		if (CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Load, NULL, 0, 0) == NULL) {
-			ExitProcess(GetLastError());
-			return FALSE;
-		}
-		//boost::thread theThread(CLoader::Initialize);
+		boost::thread theThread(&CLoader::Initialize);
 		break;
 	}
 	return TRUE;
-}
-
-void WINAPI Load()
-{
-	CLoader::Initialize();
 }
