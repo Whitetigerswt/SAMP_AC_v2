c7e670ec0efcabf24c045c6dd6e3d778070a5bd1
Whitetigerswt <spamsender123@yahoo.com>
2014-08-10 23:30:39 -0400

    Update boost to 1.56

--- ACv2_Server/CServerUpdater.cpp
+++ ACv2_Server/CServerUpdater.cpp
@@ -21,51 +21,42 @@ CServerUpdater::~CServerUpdater()
 void CServerUpdater::CheckForUpdate()
 {
 	// Get the website's response
-	Utility::Printf("check for update...");
-	try {
-		std::string updatestring = Cmd5Info::GetWebsiteText(AC_UPDATE_URL);
-		Utility::Printf("Got website text.");
-		Utility::Printf("the text is: %s", updatestring);
+	std::string updatestring = Cmd5Info::GetWebsiteText(AC_UPDATE_URL);
 
-		// Create new variables to split up the version and the update URL.
-		float version = 0.0f;
-		char* site = new char[256];
+	// Create new variables to split up the version and the update URL.
+	float version = 0.0f;
+	char* site = new char[256];
 
-		// Split up the version and update URL.
-		if (sscanf(updatestring.c_str(), "%f %s", &version, site) != EOF)
-		{
-			// If the version returned from the website is greater than the current version
-			if (version > CURRENT_VERSION)
-			{
-				// Force the user to update.
-				Utility::Printf("_____________________________________________________________");
-				Utility::Printf("");
-				Utility::Printf("SAMP_AC Server plugin is out of date, and will not continue to work after this point.");
-				Utility::Printf("The current version is %.2f and the latest version is %.2f!", CURRENT_VERSION, version);
-				Utility::Printf("You can download the latest version at: %s", site);
-				Utility::Printf("To continue using this plugin, you must update it.");
-				Utility::Printf("If you no longer wish to use this plugin, remove it from the server.cfg and adjust your scripts accordingly.");
-				Utility::Printf("The server will now close.");
-				Utility::Printf("_____________________________________________________________");
-
-				AttemptToUpdatePlugin(site);
-				exit(0);
-			}
-			else
-			{
-				Utility::Printf("_____________________________________________________________");
-				Utility::Printf("");
-				Utility::Printf("** Already up to date!");
-				Utility::Printf("");
-				Utility::Printf("_____________________________________________________________");
-			}
-		}
-		delete[] site;
-	}
-	catch (std::exception &e)
+	// Split up the version and update URL.
+	if (sscanf(updatestring.c_str(), "%f %s", &version, site) != EOF)
 	{
-		Utility::Printf("error: %s", e.what());
+		// If the version returned from the website is greater than the current version
+		if (version > CURRENT_VERSION)
+		{
+			// Force the user to update.
+			Utility::Printf("_____________________________________________________________");
+			Utility::Printf("");
+			Utility::Printf("SAMP_AC Server plugin is out of date, and will not continue to work after this point.");
+			Utility::Printf("The current version is %.2f and the latest version is %.2f!", CURRENT_VERSION, version);
+			Utility::Printf("You can download the latest version at: %s", site);
+			Utility::Printf("To continue using this plugin, you must update it.");
+			Utility::Printf("If you no longer wish to use this plugin, remove it from the server.cfg and adjust your scripts accordingly.");
+			Utility::Printf("The server will now close.");
+			Utility::Printf("_____________________________________________________________");
+
+			AttemptToUpdatePlugin(site);
+			exit(0);
+		}
+		else
+		{
+			Utility::Printf("_____________________________________________________________");
+			Utility::Printf("");
+			Utility::Printf("** Already up to date!");
+			Utility::Printf("");
+			Utility::Printf("_____________________________________________________________");
+		}
 	}
+	delete[] site;
 }
 
 void CServerUpdater::AttemptToUpdatePlugin(std::string url)
