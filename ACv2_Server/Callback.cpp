a1b5d19a015ba05521d40e37530a64dc7eb7a124
Whitetigerswt <whitetigerswt@gmail.com>
2015-02-08 22:35:49 -0500

    just-in-case check

--- ACv2_Server/Callback.cpp
+++ ACv2_Server/Callback.cpp
@@ -182,6 +182,29 @@ namespace Callback
 			{
 				// Get the player's Hardware ID.
 				hwid = ac->GetPlayerHardwareID();
+
+				if (ac->HasOnPlayerConnectCalled())
+				{
+					return true;
+				}
+				else
+				{
+					ac->SetPlayerConnected(true);
+				}
+
+				// We should NEVER get here
+
+				// Send the client the files we need them to return md5's to.
+				ac->CheckGTAFiles(playerid);
+
+				// Set defaults
+				ac->ToggleUnlimitedSprint(Callback::Default_InfSprint);
+				ac->ToggleSprintOnAllSurfaces(Callback::Default_SprintOnAllSurfaces);
+				ac->ToggleMacroLimitations(Callback::Default_MacroLimits);
+				ac->ToggleSwitchReload(Callback::Default_SwitchReload);
+				ac->ToggleCrouchBug(Callback::Default_CrouchBug);
+				ac->ToggleLiteFoot(Callback::Default_LiteFoot);
+				ac->ToggleVehicleBlips(Callback::Default_VehicleBlips);
 			}
 
 			// Check if it's an empty string
