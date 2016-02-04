71920bef74661ab65edcb3b8c1587461419f3c16
Khalid <khalidahmed333@hotmail.com>
2016-02-04 19:41:45 +0200

    Rewrote comments under OnFileCalculated callback
    This explains the changes done in commit:
    1ee06ecc6590bdf83e830f395e28759c529b75b5

--- ACv2_Server/CAntiCheat.cpp
+++ ACv2_Server/CAntiCheat.cpp
@@ -140,17 +140,31 @@ void CAntiCheat::OnMD5Calculated(int address, int size, char* md5)
 
 void CAntiCheat::OnFileCalculated(char* path, char* md5)
 {	
+	/*
+			Create a boolean variable to indicate whether a match has been found for this file in our list
+		of the MD5 of original/unmodified game files.
 
-	// Create a new variable that can contain a true/false value so we know if a file matches an MD5 from our list
+			In other words, we will set this variable to TRUE once we find a match (md5 comparison) for it
+		in our list of the MD5 of original/unmodified game files. So, setting its value to TRUE means 
+		everything is FINE. 
+		
+			But, if we do not find a match for this file in our trusted list, this means it is a modified 
+		file and the value of 'found' will remain FALSE. Hence, FALSE means this is a bad file or a cheat.
+	*/
 	bool found = false;
 
-	// Loop through a list of our md5's that we stored previously...
+	// Loop through a list of our trusted md5's that we stored previously...
 	for (std::vector<std::string>::iterator it = m_MD5s.begin(); it != m_MD5s.end(); ++it)
 	{
-		// Compare the md5 sent to us by the client to our list of MD5's
+		// Compare the md5 sent to us by the client to our list of trusted MD5's
 		if (strcmp(it->c_str(), md5) == 0)
 		{
-			// If they match, set found=true and break
+			/*
+					Okay, we found a match for our client's file in our trusted list of files. This means
+				our client is not cheating and is using original/unmodified game files. So we set the value
+				of our variable to TRUE which indicates that everything is alright.
+
+			*/
 			found = true;
 			break;
 		}
@@ -159,7 +173,7 @@ void CAntiCheat::OnFileCalculated(char* path, char* md5)
 	// If AC Main checks are enabled
 	if (Callback::GetACEnabled() == true)
 	{
-		// Check if an md5 matches, and if it doesn't kick the player.
+		// See if we have not found a match for this file in our trusted list of files. This means it's a bad file.
 		if (!found)
 		{
 			// Create a new variable holding a string that will be formatted to let the player know he's been kicked.
@@ -189,7 +203,17 @@ void CAntiCheat::OnFileCalculated(char* path, char* md5)
 			SetTimer(1000, 0, Callback::KickPlayer, (void*)ID);
 		}
 	}
-	// Execute PAWN callback.
+	/* 
+		Execute PAWN callback.
+
+		# Sidenote
+		There's a reason why we send the value of the 'found' variable reversed (i.e  !found).
+		If you read the documentation of AC_OnFileCalculated callback, you will realize that the
+		value of 'isCheat' boolean variable is TRUE when there is dangerous while 'found' variable
+		is completely contrary which is why we reverse values. 
+		(Scroll up! Documentation is written where the local variable, 'found', is declared).
+
+	*/
 	Callback::Execute("AC_OnFileCalculated", "issi", !found, md5, path, ID);
 }
 
