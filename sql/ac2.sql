70e955899b0000a9103f2a743c3bda62185a6f0b
Khalid <khalidahmed333@hotmail.com>
2016-03-15 12:48:01 +0200

    Adds timestamps to bans

--- sql/ac2.sql
+++ sql/ac2.sql
@@ -37,6 +37,7 @@ CREATE TABLE IF NOT EXISTS `bans` (
   `ServerIP` varchar(128) NOT NULL,
   `ServerName` varchar(128) NOT NULL,
   `DateTime` varchar(128) NOT NULL,
+  `Timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`SQLid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
