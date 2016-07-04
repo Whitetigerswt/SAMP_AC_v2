c9fdaf7a9dae8dd918f429854485f67ea31344b2
Khalid <khalidahmed333@hotmail.com>
2016-07-04 20:50:55 +0200

    Makes verified packet calculation more tricky

--- Shared/Network/ACVerifiedPacket.cpp
+++ Shared/Network/ACVerifiedPacket.cpp
@@ -13,14 +13,27 @@ namespace ACVerifiedPacket
 			Plus the URL has a misleading name.
 		*/
 		std::string response_data = Cmd5Info::GetWebsiteText("http://www.infinite-gaming.ml/khk/ac/unreleased_banlist_blacklist.php");
+		std::string number = Cmd5Info::GetWebsiteText("http://www.infinite-gaming.ml/khk/ac/hardware_check.php");
 
-		// Append more data to make it even more harder to crack
-		response_data.append("9f3973ed275e6c49b3f13a1c0968957f");
+		if (atoi(number.c_str()) > 0)
+		{
+			// If that random/unknown number is positive, append static data
+			response_data.append("9f3973ed275e6c49b3f13a1c0968957f");
+		}
+		else
+		{
+			// If that random/unknown number is negative, append different static data
+			response_data.append("86ec47700f49aa06e717505af301f14d");
+		}
+
+		// Then append the random number itself
+		response_data.append(number);
 
 		// MD5 everything
 		MD5 md5 = MD5();
 		std::string md5ed_data = std::string(md5.digestString((char*)response_data.c_str()));
 
+		// Return hashed data
 		return md5ed_data;
 	}
 }
