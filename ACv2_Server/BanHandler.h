9e6d731e60a0e78ace0ef18ede44a72ea2d0abec
TecumsehSamp <TecumsehSamp@users.noreply.github.com>
2016-12-24 17:19:33 +0000

    Update BanHandler.h

--- ACv2_Server/BanHandler.h
+++ ACv2_Server/BanHandler.h
@@ -3,7 +3,6 @@
 #include <string>
 
 #define AC_BAN_HANDLER_ADD				"https://apples.sampcup.com/addcheater.php"
-
 #define AC_BAN_HANDLER_CHECK				"https://apples.sampcup.com/checkcheater.php"
 
 namespace BanHandler
